096091e5dbf8522641cd9e245b2af3730ae4d69c
Shaurya Pundir <anjanavikrampundir@gmail.com>
2025-05-19 10:23:52 +0530

    1) Done the component of the UI fully functional and fixed error related to no output which took very much time . 2)Done automatic testing using test.mjs ,and while testing it fixed a lot of bugs.

--- cpp/p1.cpp
+++ cpp/p1.cpp
@@ -1,93 +1,94 @@
-// #include <iostream>
-// #include <sstream>
-// #include <vector>
-// #include <map>
-// #include <stack>
-// #include <fstream>
-// #include <stdexcept>
-// #include <regex>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <fstream>
+#include <stdexcept>
+#include <regex>
+#include <math.h>
 
-// using namespace std;
+using namespace std;
 
-// struct SymbolInfo {
-//     string type;
-//     int value;
-//     string scope;
-// };
+struct SymbolInfo {
+    string type;
+    int value;
+    string scope;
+};
 
-// // ✅ Global symbol table
-// map<string, SymbolInfo> symbolTable;
 
-// struct TreeNode {
-//     string value;
-//     TreeNode* left;
-//     TreeNode* right;
-// };
+map<string, SymbolInfo> symbolTable;
 
-// bool isOperator(const string& token) {
-//     return token == "+" || token == "-" || token == "*" || token == "/";
-// }
+struct TreeNode {
+    string value;
+    TreeNode* left;
+    TreeNode* right;
+};
 
-// int precedence(const string& op) {
-//     if (op == "+" || op == "-") return 1;
-//     if (op == "*" || op == "/") return 2;
-//     return 0;
-// }
+bool isOperator(const string& token) {
+    return token == "+" || token == "-" || token == "*" || token == "/";
+}
 
-// vector<string> tokenizeExpression(const string& expr) {
-//     vector<string> tokens;
-//     string token;
-//     for (size_t i = 0; i < expr.length(); ++i) {
-//         char ch = expr[i];
-//         if (isspace(ch)) continue;
-//         if (isdigit(ch)) {
-//             token.clear();
-//             while (i < expr.length() && isdigit(expr[i])) token += expr[i++];
-//             --i;
-//             tokens.push_back(token);
-//         } else if (isalpha(ch)) {
-//             token.clear();
-//             while (i < expr.length() && (isalnum(expr[i]) || expr[i] == '_')) token += expr[i++];
-//             --i;
-//             tokens.push_back(token);
-//         } else {
-//             tokens.push_back(string(1, ch));
-//         }
-//     }
-//     return tokens;
-// }
+int precedence(const string& op) {
+    if (op == "+" || op == "-") return 1;
+    if (op == "*" || op == "/") return 2;
+    return 0;
+}
 
-// vector<string> infixToPostfix(const vector<string>& infix) {
-//     stack<string> opStack;
-//     vector<string> postfix;
+vector<string> tokenizeExpression(const string& expr) {
+    vector<string> tokens;
+    string token;
+    for (size_t i = 0; i < expr.length(); ++i) {
+        char ch = expr[i];
+        if (isspace(ch)) continue;
+        if (isdigit(ch)) {
+            token.clear();
+            while (i < expr.length() && isdigit(expr[i])) token += expr[i++];
+            --i;
+            tokens.push_back(token);
+        } else if (isalpha(ch)) {
+            token.clear();
+            while (i < expr.length() && (isalnum(expr[i]) || expr[i] == '_')) token += expr[i++];
+            --i;
+            tokens.push_back(token);
+        } else {
+            tokens.push_back(string(1, ch));
+        }
+    }
+    return tokens;
+}
 
-//     for (const string& token : infix) {
-//         if (isalpha(token[0]) || isdigit(token[0])) {
-//             postfix.push_back(token);
-//         } else if (isOperator(token)) {
-//             while (!opStack.empty() && precedence(opStack.top()) >= precedence(token)) {
-//                 postfix.push_back(opStack.top());
-//                 opStack.pop();
-//             }
-//             opStack.push(token);
-//         } else if (token == "(") {
-//             opStack.push(token);
-//         } else if (token == ")") {
-//             while (!opStack.empty() && opStack.top() != "(") {
-//                 postfix.push_back(opStack.top());
-//                 opStack.pop();
-//             }
-//             if (!opStack.empty()) opStack.pop(); // remove '('
-//         }
-//     }
+vector<string> infixToPostfix(const vector<string>& infix) {
+    stack<string> opStack;
+    vector<string> postfix;
 
-//     while (!opStack.empty()) {
-//         postfix.push_back(opStack.top());
-//         opStack.pop();
-//     }
+    for (const string& token : infix) {
+        if (isalpha(token[0]) || isdigit(token[0])) {
+            postfix.push_back(token);
+        } else if (isOperator(token)) {
+            while (!opStack.empty() && precedence(opStack.top()) >= precedence(token)) {
+                postfix.push_back(opStack.top());
+                opStack.pop();
+            }
+            opStack.push(token);
+        } else if (token == "(") {
+            opStack.push(token);
+        } else if (token == ")") {
+            while (!opStack.empty() && opStack.top() != "(") {
+                postfix.push_back(opStack.top());
+                opStack.pop();
+            }
+            if (!opStack.empty()) opStack.pop(); // remove '('
+        }
+    }
 
-//     return postfix;
-// }
+    while (!opStack.empty()) {
+        postfix.push_back(opStack.top());
+        opStack.pop();
+    }
+
+    return postfix;
+}
 
 // int evaluateExpression(const vector<string>& postfix) {
 //     stack<int> st;
@@ -112,158 +113,183 @@
 //     return st.top();
 // }
 
-// TreeNode* buildParseTreeWithAssignment(const string& varName, const vector<string>& postfix) {
-//     stack<TreeNode*> st;
-//     for (const string& token : postfix) {
-//         if (isOperator(token)) {
-//             TreeNode* right = st.top(); st.pop();
-//             TreeNode* left = st.top(); st.pop();
-//             st.push(new TreeNode{token, left, right});
-//         } else {
-//             st.push(new TreeNode{token, nullptr, nullptr});
-//         }
-//     }
-//     TreeNode* exprTree = st.top(); st.pop();
-//     TreeNode* assignNode = new TreeNode{"=", new TreeNode{varName, nullptr, nullptr}, exprTree};
-//     return assignNode;
-// }
+int evaluateExpression(const vector<string>& postfix) {
+    stack<int> st;
+    for (const string& token : postfix) {
+        if (isdigit(token[0])) {
+            st.push(stoi(token));
+        } else if (isalpha(token[0])) {
+            if (symbolTable.find(token) != symbolTable.end()) {
+                st.push(symbolTable[token].value);
+            } else {
+                throw runtime_error("Variable '" + token + "' not declared in current scope.");
+            }
+        } else {
+            int b = st.top(); st.pop();
+            int a = st.top(); st.pop();
+            if (token == "+") st.push(a + b);
+            else if (token == "-") st.push(a - b);
+            else if (token == "*") st.push(a * b);
+            else if (token == "/") st.push(a / b);
+            else if (token == "%") st.push(a % b);
+            else if (token == "^") st.push(static_cast<int>(pow(a, b))); // Explicitly use std::pow and cast to int
+        }
+    }
+    return st.top();
+}
 
-// void visualizeParseTree(TreeNode* root, const string& filename) {
-//     ofstream fout(filename);
-//     fout << "digraph ParseTree {\n";
-//     int id = 0;
-//     map<TreeNode*, int> ids;
+TreeNode* buildParseTreeWithAssignment(const string& varName, const vector<string>& postfix) {
+    stack<TreeNode*> st;
+    for (const string& token : postfix) {
+        if (isOperator(token)) {
+            TreeNode* right = st.top(); st.pop();
+            TreeNode* left = st.top(); st.pop();
+            st.push(new TreeNode{token, left, right});
+        } else {
+            st.push(new TreeNode{token, nullptr, nullptr});
+        }
+    }
+    TreeNode* exprTree = st.top(); st.pop();
+    TreeNode* assignNode = new TreeNode{"=", new TreeNode{varName, nullptr, nullptr}, exprTree};
+    return assignNode;
+}
 
-//     function<void(TreeNode*)> dfs = [&](TreeNode* node) {
-//         if (!node) return;
-//         ids[node] = id++;
-//         fout << "  " << ids[node] << " [label=\"" << node->value << "\"];\n";
-//         if (node->left) {
-//             dfs(node->left);
-//             fout << "  " << ids[node] << " -> " << ids[node->left] << ";\n";
-//         }
-//         if (node->right) {
-//             dfs(node->right);
-//             fout << "  " << ids[node] << " -> " << ids[node->right] << ";\n";
-//         }
-//     };
+void visualizeParseTree(TreeNode* root, const string& filename) {
+    ofstream fout(filename);
+    fout << "digraph ParseTree {\n";
+    int id = 0;
+    map<TreeNode*, int> ids;
 
-//     dfs(root);
-//     fout << "}\n";
-//     fout.close();
-//     cout << "Parse Tree saved as " << filename << endl;
-// }
+    function<void(TreeNode*)> dfs = [&](TreeNode* node) {
+        if (!node) return;
+        ids[node] = id++;
+        fout << "  " << ids[node] << " [label=\"" << node->value << "\"];\n";
+        if (node->left) {
+            dfs(node->left);
+            fout << "  " << ids[node] << " -> " << ids[node->left] << ";\n";
+        }
+        if (node->right) {
+            dfs(node->right);
+            fout << "  " << ids[node] << " -> " << ids[node->right] << ";\n";
+        }
+    };
 
-// void declareVariables(const string& declaration) {
-//     regex pattern(R"(int\s+([\w\s=,]+);)");
-//     smatch match;
+    dfs(root);
+    fout << "}\n";
+    fout.close();
+    cout << "Parse Tree saved as " << filename << endl;
+}
+
+void declareVariables(const string& declaration) {
+    regex pattern(R"(int\s+([\w\s=,]+);)");
+    smatch match;
     
-//     if (!regex_search(declaration, match, pattern)) {
-//         cout << "Only 'int' type declarations supported.\n";
-//         return;
-//     }
+    if (!regex_search(declaration, match, pattern)) {
+        cout << "Only 'int' type declarations supported.\n";
+        return;
+    }
 
-//     string vars = match[1];
-//     stringstream ss(vars);
-//     string token;
+    string vars = match[1];
+    stringstream ss(vars);
+    string token;
 
-//     while (getline(ss, token, ',')) {
-//         string name;
-//         int value = 0;
+    while (getline(ss, token, ',')) {
+        string name;
+        int value = 0;
 
-//         size_t eq = token.find('=');
-//         if (eq != string::npos) {
-//             name = token.substr(0, eq);
-//             value = stoi(token.substr(eq + 1));
-//         } else {
-//             name = token;
-//         }
+        size_t eq = token.find('=');
+        if (eq != string::npos) {
+            name = token.substr(0, eq);
+            value = stoi(token.substr(eq + 1));
+        } else {
+            name = token;
+        }
 
-//         name = regex_replace(name, regex("^\\s+|\\s+$"), ""); // Trim
-//         cout << "Declaring variable: " << name << " = " << value << endl;
-//         symbolTable[name] = {"int", value, "Global"};
-//     }
+        name = regex_replace(name, regex("^\\s+|\\s+$"), ""); // Trim
+        cout << "Declaring variable: " << name << " = " << value << endl;
+        symbolTable[name] = {"int", value, "Global"};
+    }
 
-//     cout << "Variables declared.\n";
-// }
+    cout << "Variables declared.\n";
+}
 
-// void displaySymbolTable() {
-//     cout << "\nSymbol Table:\n";
-//     cout << "-------------------------------------------\n";
-//     cout << "Name\tType\tValue\tScope\n";
-//     cout << "-------------------------------------------\n";
-//     for (const auto& entry : symbolTable) {
-//         cout << entry.first << "\t" << entry.second.type << "\t" << entry.second.value << "\t" << entry.second.scope << "\n";
-//     }
-//     cout << "-------------------------------------------\n";
-// }
+void displaySymbolTable() {
+    cout << "\nSymbol Table:\n";
+    cout << "-------------------------------------------\n";
+    cout << "Name\tType\tValue\tScope\n";
+    cout << "-------------------------------------------\n";
+    for (const auto& entry : symbolTable) {
+        cout << entry.first << "\t" << entry.second.type << "\t" << entry.second.value << "\t" << entry.second.scope << "\n";
+    }
+    cout << "-------------------------------------------\n";
+}
 
-// int main() {
-//     int choice;
-//     string input;
+int main() {
+    int choice;
+    string input;
 
-//     while (true) {
-//         cout << "\nMenu:\n";
-//         cout << "1. Enter Expression\n";
-//         cout << "2. Declare Variables\n";
-//         cout << "3. Display Symbol Table\n";
-//         cout << "4. Visualize Parse Tree\n";
-//         cout << "5. Exit\n";
-//         cout << "Enter your choice: ";
-//         cin >> choice;
-//         cin.ignore();
+    while (true) {
+        cout << "\nMenu:\n";
+        cout << "1. Enter Expression\n";
+        cout << "2. Declare Variables\n";
+        cout << "3. Display Symbol Table\n";
+        cout << "4. Visualize Parse Tree\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
 
-//         if (choice == 1) {
-//             cout << "Enter an expression ending with ';': ";
-//             getline(cin, input);
-//             stringstream ss(input);
-//             string statement;
+        if (choice == 1) {
+            cout << "Enter an expression ending with ';': ";
+            getline(cin, input);
+            stringstream ss(input);
+            string statement;
 
-//             while (getline(ss, statement, ';')) {
-//                 if (statement.empty()) continue;
+            while (getline(ss, statement, ';')) {
+                if (statement.empty()) continue;
 
-//                 vector<string> tokens = tokenizeExpression(statement);
-//                 if (tokens.size() > 2 && tokens[1] == "=") {
-//                     string varName = tokens[0];
-//                     if (symbolTable.find(varName) == symbolTable.end()) {
-//                         cerr << "Error: Variable '" << varName << "' not declared.\n";
-//                         continue;
-//                     }
+                vector<string> tokens = tokenizeExpression(statement);
+                if (tokens.size() > 2 && tokens[1] == "=") {
+                    string varName = tokens[0];
+                    if (symbolTable.find(varName) == symbolTable.end()) {
+                        cerr << "Error: Variable '" << varName << "' not declared.\n";
+                        continue;
+                    }
 
-//                     tokens.erase(tokens.begin(), tokens.begin() + 2);
-//                     vector<string> postfix = infixToPostfix(tokens);
+                    tokens.erase(tokens.begin(), tokens.begin() + 2);
+                    vector<string> postfix = infixToPostfix(tokens);
 
-//                     try {
-//                         int result = evaluateExpression(postfix);
-//                         symbolTable[varName].value = result;
-//                         cout << "Assigned " << varName << " = " << result << endl;
+                    try {
+                        int result = evaluateExpression(postfix);
+                        symbolTable[varName].value = result;
+                        cout << "Assigned " << varName << " = " << result << endl;
 
-//                         TreeNode* root = buildParseTreeWithAssignment(varName, postfix);
-//                         visualizeParseTree(root, "parse_tree.dot");
-//                     } catch (exception& e) {
-//                         cerr << "Error evaluating expression: " << e.what() << endl;
-//                     }
-//                 } else {
-//                     cout << "Invalid expression: " << statement << endl;
-//                 }
-//             }
+                        TreeNode* root = buildParseTreeWithAssignment(varName, postfix);
+                        visualizeParseTree(root, "parse_tree.dot");
+                    } catch (exception& e) {
+                        cerr << "Error evaluating expression: " << e.what() << endl;
+                    }
+                } else {
+                    cout << "Invalid expression: " << statement << endl;
+                }
+            }
 
-//         } else if (choice == 2) {
-//             cout << "Enter declaration (e.g., int a, b = 2;): ";
-//             getline(cin, input);
-//             declareVariables(input);
-//         } else if (choice == 3) {
-//             displaySymbolTable();
-//         } else if (choice == 4) {
-//             system("dot -Tpng parse_tree.dot -o parse_tree.png");
-//             system("start parse_tree.png"); // For Windows
-//         } else if (choice == 5) {
-//             break;
-//         } else {
-//             cout << "Invalid choice.\n";
-//         }
-//     }
+        } else if (choice == 2) {
+            cout << "Enter declaration (e.g., int a, b = 2;): ";
+            getline(cin, input);
+            declareVariables(input);
+        } else if (choice == 3) {
+            displaySymbolTable();
+        } else if (choice == 4) {
+            system("dot -Tpng parse_tree.dot -o parse_tree.png");
+            system("start parse_tree.png"); // For Windows
+        } else if (choice == 5) {
+            break;
+        } else {
+            cout << "Invalid choice.\n";
+        }
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
--- server.js
+++ server.js
@@ -1,14 +1,96 @@
-const express = require('express');
-const { spawn } = require('child_process');
-const path = require('path');
+// const express = require('express');
+// const { spawn } = require('child_process');
+// const path = require('path');
+// const app = express();
+// const port = 3000;
+
+// app.use(express.static('public'));
+// app.use(express.json());
+
+// const cppDir = path.join(__dirname, 'cpp');
+// const cppExecutable = path.join(cppDir, process.platform === 'win32' ? 'p1.exe' : 'p1');
+
+// // Spawn the C++ process once at server start
+// const cppProcess = spawn(cppExecutable, { cwd: cppDir });
+// let outputBuffer = '';
+
+// // Handle stdout
+// cppProcess.stdout.on('data', (data) => {
+//     outputBuffer += data.toString();
+// });
+
+// cppProcess.stderr.on('data', (data) => {
+//     console.error(`C++ Error: ${data}`);
+// });
+
+// cppProcess.on('close', (code) => {
+//     console.log(`C++ process exited with code ${code}`);
+// });
+
+// app.post('/execute', (req, res) => {
+//     const { choice, input } = req.body;
+//     outputBuffer = ''; // Reset buffer for new command
+
+//     // Send command to C++ process
+//     cppProcess.stdin.write(`${choice}\n`);
+//     if (input) {
+//         cppProcess.stdin.write(`${input}\n`);
+//     }
+
+//     // Wait briefly for output (adjust delay as needed)
+//     setTimeout(() => {
+//         if (outputBuffer) {
+//             res.json({ output: outputBuffer });
+//         } else {
+//             res.status(500).json({ error: 'No output received' });
+//         }
+//     }, 100); // 100ms delay to allow C++ to process
+// });
+
+// app.get('/visualize', (req, res) => {
+//     const dotFile = path.join(cppDir, 'parse_tree.dot');
+//     const pngFile = path.join(cppDir, 'parse_tree.png');
+//     const dotProcess = spawn('dot', ['-Tpng', dotFile, '-o', pngFile], { cwd: cppDir });
+
+//     dotProcess.on('close', (code) => {
+//         if (code === 0) {
+//             res.sendFile(pngFile);
+//         } else {
+//             res.status(500).json({ error: 'Failed to generate parse tree image' });
+//         }
+//     });
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+// // Graceful shutdown
+// process.on('SIGINT', () => {
+//     cppProcess.stdin.write('5\n'); // Send exit command (assuming 5 exits the loop)
+//     cppProcess.on('close', () => process.exit());
+// });
+
+
+
+// server.js
+import express from 'express';
+import { spawn } from 'child_process';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
 
-const cppDir = path.join(__dirname, 'cpp');
-const cppExecutable = path.join(cppDir, process.platform === 'win32' ? 'p1.exe' : 'p1');
+const cppDir = join(__dirname, 'cpp');
+const cppExecutable = join(cppDir, process.platform === 'win32' ? 'p1.exe' : 'p1');
 
 // Spawn the C++ process once at server start
 const cppProcess = spawn(cppExecutable, { cwd: cppDir });
@@ -27,7 +109,26 @@ cppProcess.on('close', (code) => {
     console.log(`C++ process exited with code ${code}`);
 });
 
-app.post('/execute', (req, res) => {
+// app.post('/execute', async (req, res) => {
+//     const { choice, input } = req.body;
+//     outputBuffer = ''; // Reset buffer for new command
+
+//     // Send command to C++ process
+//     cppProcess.stdin.write(`${choice}\n`);
+//     if (input) {
+//         cppProcess.stdin.write(`${input}\n`);
+//     }
+
+//     // Wait briefly for output (adjust delay as needed)
+//     await new Promise(resolve => setTimeout(resolve, 100)); // 100ms delay to allow C++ to process
+//     if (outputBuffer) {
+//         res.json({ output: outputBuffer });
+//     } else {
+//         res.status(500).json({ error: 'No output received' });
+//     }
+// });
+
+app.post('/execute', async (req, res) => {
     const { choice, input } = req.body;
     outputBuffer = ''; // Reset buffer for new command
 
@@ -38,18 +139,17 @@ app.post('/execute', (req, res) => {
     }
 
     // Wait briefly for output (adjust delay as needed)
-    setTimeout(() => {
-        if (outputBuffer) {
-            res.json({ output: outputBuffer });
-        } else {
-            res.status(500).json({ error: 'No output received' });
-        }
-    }, 100); // 100ms delay to allow C++ to process
+    await new Promise(resolve => setTimeout(resolve, 500)); // 100ms delay to allow C++ to process
+    if (outputBuffer) {
+        res.json({ output: outputBuffer });
+    } else {
+        res.status(500).json({ error: 'No output received' });
+    }
 });
 
-app.get('/visualize', (req, res) => {
-    const dotFile = path.join(cppDir, 'parse_tree.dot');
-    const pngFile = path.join(cppDir, 'parse_tree.png');
+app.get('/visualize', async (req, res) => {
+    const dotFile = join(cppDir, 'parse_tree.dot');
+    const pngFile = join(cppDir, 'parse_tree.png');
     const dotProcess = spawn('dot', ['-Tpng', dotFile, '-o', pngFile], { cwd: cppDir });
 
     dotProcess.on('close', (code) => {
@@ -67,6 +167,6 @@ app.listen(port, () => {
 
 // Graceful shutdown
 process.on('SIGINT', () => {
-    cppProcess.stdin.write('5\n'); // Send exit command (assuming 5 exits the loop)
+    cppProcess.stdin.write('7\n'); // Updated to choice 7 (exit) based on new menu
     cppProcess.on('close', () => process.exit());
 });
